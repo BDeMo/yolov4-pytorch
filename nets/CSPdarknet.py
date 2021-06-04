2434afc88d0890bdb19c5655bb7c577d22bf18d3
samj <samj>
2021-06-04 15:19:11 +0800

    fix format

--- nets/CSPdarknet.py
+++ nets/CSPdarknet.py
@@ -5,9 +5,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-# -------------------------------------------------#
-#   MISH激活函数
-# -------------------------------------------------#
 class Mish(nn.Module):
     def __init__(self):
         super(Mish, self).__init__()
@@ -16,10 +13,6 @@ class Mish(nn.Module):
         return x * torch.tanh(F.softplus(x))
 
 
-# ---------------------------------------------------#
-#   卷积块 -> 卷积 + 标准化 + 激活函数
-#   Conv2d + BatchNormalization + Mish
-# ---------------------------------------------------#
 class BasicConv(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, stride=1):
         super(BasicConv, self).__init__()
@@ -35,10 +28,6 @@ class BasicConv(nn.Module):
         return x
 
 
-# ---------------------------------------------------#
-#   CSPdarknet的结构块的组成部分
-#   内部堆叠的残差块
-# ---------------------------------------------------#
 class Resblock(nn.Module):
     def __init__(self, channels, hidden_channels=None):
         super(Resblock, self).__init__()
@@ -55,30 +44,13 @@ class Resblock(nn.Module):
         return x + self.block(x)
 
 
-# --------------------------------------------------------------------#
-#   CSPdarknet的结构块
-#   首先利用ZeroPadding2D和一个步长为2x2的卷积块进行高和宽的压缩
-#   然后建立一个大的残差边shortconv、这个大残差边绕过了很多的残差结构
-#   主干部分会对num_blocks进行循环，循环内部是残差结构。
-#   对于整个CSPdarknet的结构块，就是一个大残差块+内部多个小残差块
-# --------------------------------------------------------------------#
 class Resblock_body(nn.Module):
     def __init__(self, in_channels, out_channels, num_blocks, first):
         super(Resblock_body, self).__init__()
-        # ----------------------------------------------------------------#
-        #   利用一个步长为2x2的卷积块进行高和宽的压缩
-        # ----------------------------------------------------------------#
         self.downsample_conv = BasicConv(in_channels, out_channels, 3, stride=2)
 
         if first:
-            # --------------------------------------------------------------------------#
-            #   然后建立一个大的残差边self.split_conv0、这个大残差边绕过了很多的残差结构
-            # --------------------------------------------------------------------------#
             self.split_conv0 = BasicConv(out_channels, out_channels, 1)
-
-            # ----------------------------------------------------------------#
-            #   主干部分会对num_blocks进行循环，循环内部是残差结构。
-            # ----------------------------------------------------------------#
             self.split_conv1 = BasicConv(out_channels, out_channels, 1)
             self.blocks_conv = nn.Sequential(
                 Resblock(channels=out_channels, hidden_channels=out_channels // 2),
@@ -87,14 +59,7 @@ class Resblock_body(nn.Module):
 
             self.concat_conv = BasicConv(out_channels * 2, out_channels, 1)
         else:
-            # --------------------------------------------------------------------------#
-            #   然后建立一个大的残差边self.split_conv0、这个大残差边绕过了很多的残差结构
-            # --------------------------------------------------------------------------#
             self.split_conv0 = BasicConv(out_channels, out_channels // 2, 1)
-
-            # ----------------------------------------------------------------#
-            #   主干部分会对num_blocks进行循环，循环内部是残差结构。
-            # ----------------------------------------------------------------#
             self.split_conv1 = BasicConv(out_channels, out_channels // 2, 1)
             self.blocks_conv = nn.Sequential(
                 *[Resblock(out_channels // 2) for _ in range(num_blocks)],
@@ -105,47 +70,26 @@ class Resblock_body(nn.Module):
 
     def forward(self, x):
         x = self.downsample_conv(x)
-
         x0 = self.split_conv0(x)
-
         x1 = self.split_conv1(x)
         x1 = self.blocks_conv(x1)
-
-        # ------------------------------------#
-        #   将大残差边再堆叠回来
-        # ------------------------------------#
         x = torch.cat([x1, x0], dim=1)
-        # ------------------------------------#
-        #   最后对通道数进行整合
-        # ------------------------------------#
         x = self.concat_conv(x)
-
         return x
 
 
-# ---------------------------------------------------#
-#   CSPdarknet53 的主体部分
-#   输入为一张416x416x3的图片
-#   输出为三个有效特征层
-# ---------------------------------------------------#
 class CSPDarkNet(nn.Module):
     def __init__(self, layers, num_class=1000):
         super(CSPDarkNet, self).__init__()
         self.inplanes = 32
-        # 416,416,3 -> 416,416,32
         self.pre = BasicConv(3, self.inplanes, kernel_size=3, stride=1)
         self.feature_channels = [64, 128, 256, 512, 1024]
 
         self.stages = nn.ModuleList([
-            # 416,416,32 -> 208,208,64
             Resblock_body(self.inplanes, self.feature_channels[0], layers[0], first=True),
-            # 208,208,64 -> 104,104,128
             Resblock_body(self.feature_channels[0], self.feature_channels[1], layers[1], first=False),
-            # 104,104,128 -> 52,52,256
             Resblock_body(self.feature_channels[1], self.feature_channels[2], layers[2], first=False),
-            # 52,52,256 -> 26,26,512
             Resblock_body(self.feature_channels[2], self.feature_channels[3], layers[3], first=False),
-            # 26,26,512 -> 13,13,1024
             Resblock_body(self.feature_channels[3], self.feature_channels[4], layers[4], first=False)
         ])
         self.fc = nn.Linear(1024, num_class)
@@ -180,6 +124,7 @@ def darknet53(pretrained, num_class, **kwargs):
         else:
             raise Exception("darknet request a pretrained path. got [{}]".format(pretrained))
     return model
+
 
 if __name__ == "__main__":
     model = darknet53(None)
--- nets/MobileNet.py
+++ nets/MobileNet.py
@@ -5,7 +5,6 @@ import torch.nn.functional as F
 
 
 class Block(nn.Module):
-    '''Depthwise conv + Pointwise conv'''
 
     def __init__(self, in_planes, out_planes, stride=1):
         super(Block, self).__init__()
@@ -25,8 +24,6 @@ class Block(nn.Module):
 
 
 class MobileNet(nn.Module):
-    # (128,2) means conv planes=128, conv stride=2,
-    # by default conv stride=1
     cfg1 = [64, (128, 2), 128, (256, 2), (256, 2)]
     cfg2 = [(512, 2), 512, 512, 512, 512, 512, ]
     cfg3 = [(1024, 2), 1024]
@@ -36,9 +33,11 @@ class MobileNet(nn.Module):
         self.pre = nn.Sequential(nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1, bias=False),
                                  nn.BatchNorm2d(32),
                                  nn.ReLU())
-        self.stage1 = self._make_layers(self.cfg1, in_planes=32)
-        self.stage2 = self._make_layers(self.cfg2, in_planes=256)
-        self.stage3 = self._make_layers(self.cfg3, in_planes=512)
+        self.stages = nn.ModuleList([
+            self._make_layers(self.cfg1, in_planes=32),
+            self._make_layers(self.cfg2, in_planes=256),
+            self._make_layers(self.cfg3, in_planes=512)
+        ])
         self.linear = nn.Linear(1024 * 6 * 6, num_classes)
 
     def _make_layers(self, cfg, in_planes):
@@ -51,33 +50,28 @@ class MobileNet(nn.Module):
         return nn.Sequential(*layers)
 
     def forward(self, x):
-        #         x = F.relu(self.bn1(self.conv1(x)))
         x = self.pre(x)
-        out1 = self.stage1(x)
-        out2 = self.stage2(out1)
-        out3 = self.stage3(out2)
+        out1 = self.stages[0](x)
+        out2 = self.stages[1](out1)
+        out3 = self.stages[2](out2)
         x = F.avg_pool2d(out3, 2)
         x = x.view(x.size(0), -1)
-        #         x = self.linear(x)
+        x = self.linear(x)
         return x, out1, out2, out3
 
+
 def mobilenet(pretrained):
-    model = MobileNet()
+    model = MobileNet(num_classes=10)
     if pretrained:
         if isinstance(pretrained, str):
-            model_dict = model.state_dict()
-            device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-            pretrained_dict = torch.load(pretrained, map_location=device)
-            pretrained_dict = {k: v for k, v in pretrained_dict.items() if np.shape(model_dict[k]) == np.shape(v)}
-            model_dict.update(pretrained_dict)
-            model.load_state_dict(model_dict)
+            model.load_state_dict(torch.load(pretrained))
         else:
-            raise Exception("resnet request a pretrained path. got [{}]".format(pretrained))
+            raise Exception("mobilenet request a pretrained path. got [{}]".format(pretrained))
     return model
+
 
 if __name__ == "__main__":
     model = mobilenet(None)
     x = torch.randn(1, 3, 416, 416)
     _, out1, out2, out3 = model(x)
-    # print(net.layers)
     print(_.size(), out1.shape, out2.shape, out3.shape)
